9c571626987e2c29e18d592b5aa95d87f4039de4
= <achyutamgrd@gmail.com>
2024-05-18 00:31:56 +0530

    add css in loading page

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <Suspense fallback ={<> Loading...</>}>
+    <Suspense fallback ={<div className='text-6xl grid place-content-center bg-zinc-700 text-amber-50 font-mono'> Loading...</div>}>
       <App />
     </Suspense>
   </React.StrictMode>,
