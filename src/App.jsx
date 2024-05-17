e195fd0d179d7b6b7b66eab6356baf9e83ce9ee5
= <achyutamgrd@gmail.com>
2024-05-18 00:19:26 +0530

    css changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
     <div className='mx-0 p-3 bg-gray-900 text-white text-2xl'>
-        <a href="https://akshutam-portfolio.vercel.app/">Akshutam.dev</a>
+        <a className='px-6' href="https://akshutam-portfolio.vercel.app/">Akshutam.dev</a>
       </div>
     <div className=' mx-auto p-3 bg-gray-600 text-white h-screen w-full grid place-content-center'>
       <Counter />
